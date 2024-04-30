1b0850c7ce7825466be7f697cc2c1d75e0d6f946
Antony Lao <antony.lao@laposte.net>
2024-04-30 18:38:05 +0200

    delete unused env vars in vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,8 +3,6 @@
 interface ImportMetaEnv {
   readonly VITE_API_BASE_URL_PROD: string,
   readonly VITE_API_BASE_URL_DEV: string,
-  readonly VITE_TOKEN: string,
-  readonly VITE_REFRESH_TOKEN: string,
   // more env variables...
 }
 
