4e0ab4d4e9a023f9a095eef72724707551efaa51
Antony <antony.lao@laposte.net>
2026-02-26 12:46:09 +0100

    debug: api event: log function name in error log

--- src/services/api/events.ts
+++ src/services/api/events.ts
@@ -4,7 +4,7 @@ import { fakerEvents } from "../../VOLUNTEER_FRONT/Pages/Event/fakerEvents";
 import { RoutesBack } from "../utils/RoutesBackUtils";
 const api = useApi();
 
-
+const logs = true
 export async function getEvents() {
   try {
     // const { data } = await api.get("posts");
@@ -14,8 +14,11 @@ export async function getEvents() {
     return fakerEvents.datas
 
   } catch (err) {
-    console.log("ERROR")
-    console.log(err)
+    if (logs) {
+      console.log(String.fromCodePoint(0x1F516) + " events.ts ~ getEvents: ")
+      console.log("ERROR")
+      console.log(err)
+    }
   }
 }
 
@@ -30,8 +33,11 @@ export async function getUpcomingEvents() {
     return fakerEvents.datas.filter((obj) => parse(obj.startOn, "YYYY-MM-DD HH:MM:SS") < new Date())
 
   } catch (err) {
-    console.log("ERROR")
-    console.log(err)
+    if (logs) {
+      console.log(String.fromCodePoint(0x1F516) + " events.ts ~ getUpcomingEvents: ")
+      console.log("ERROR")
+      console.log(err)
+    }
   }
 }
 
@@ -44,8 +50,11 @@ export async function getFinishedEvents() {
     return fakerEvents.datas.filter((obj) => parse(obj.start_on, "YYYY-MM-DD HH:MM:SS") < new Date())
 
   } catch (err) {
-    console.log("ERROR")
-    console.log(err)
+    if (logs) {
+      console.log(String.fromCodePoint(0x1F516) + " events.ts ~ getFinishedEvents: ")
+      console.log("ERROR")
+      console.log(err)
+    }
   }
 }
 
@@ -55,7 +64,10 @@ export async function getEventById(id: number) {
     const { data } = await api.get(`posts/${id}`);
     return data;
   } catch (err) {
-    console.log("ERROR")
-    console.log(err)
+    if (logs) {
+      console.log(String.fromCodePoint(0x1F516) + " events.ts ~ getEventById: ")
+      console.log("ERROR")
+      console.log(err)
+    }
   }
 }
